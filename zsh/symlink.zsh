1cc948c46f0874fc54074b4feb0d469957d4192a
Addison Omead Amiri <addisonamiri@gmail.com>
2013-10-22 00:49:28 -0400

    added zsh

---
+++ zsh/symlink.zsh
@@ -0,0 +1,26 @@
+#! /usr/bin/zsh
+
+if [[ -e ~/.oh-my-zsh ]]; then
+  if [[ ! ( -d ~/.oh-my-zsh ) ]]; then
+    echo "There's a ~/.oh-my-zsh and it's not a directory..."; exit 1
+  else
+    if [[ -e ~/.oh-my-zsh/themes ]]; then
+      if [[ ! ( -d ~/.oh-my-zsh/themes ) ]]; then
+        echo "There's a ~/.oh-my-zsh/themes and it's not a directory..."; exit 1
+      else
+        if [[ -e ~/.oh-my-zsh/themes/thecoolest.zsh-theme ]]; then
+          echo "There's already a thecoolest.zsh-theme in ~/.oh-my-zsh/themes\nIt has been renamed thecoolest.zsh-theme.old"
+          mv ~/.oh-my-zsh/themes/thecoolest.zsh-theme ~/.oh-my-zsh/themes/thecoolest.zsh-theme.old
+        fi
+      fi
+    else
+      mkdir ~/.oh-my-zsh/themes
+    fi
+  fi
+else
+  mkdir -p ~/.oh-my-zsh/themes
+fi
+
+mypath=$(exec 2>/dev/null;cd $(dirname $0);unset PWD;/usr/bin/pwd||/bin/pwd||pwd)
+ln -s $mypath/zshrc ~/.zshrc
+ln -s $mypath/theme ~/.oh-my-zsh/themes/thecoolest.zsh-theme
